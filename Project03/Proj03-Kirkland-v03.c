dd9cbb157509bda3fba96e481f1608120b687129
Myrsine <grantmkirkland@gmail.com>
2024-02-24 20:08:39 -0700

    Working password detection. Working pattern B with timer

--- Project03/Proj03-Kirkland-v03.c
+++ Project03/Proj03-Kirkland-v03.c
@@ -9,7 +9,8 @@
 	Version Summary:
         v01: Press button in column to toggle 0-3 binary on leds
         v02: Working LED Output, defined keypad macros, working column readout
-
+        v03: Working keypad password. Working Pattern B. 
+        
     Ports Map: 
         P3.1, 3.5, 1.1, 5.4, 1.4, 5.3, 5.1, 5.0 - Keyboard P1-8
         P3.0, 2.5, 4.4, 4.7, 4.6, 4.0, 2.2, 2.0 - LEDs 0-7
@@ -27,7 +28,7 @@
         P5 |4|  |5|  |6|  |B| P5.3
         P6 |7|  |8|  |9|  |C| P5.1
         P7 |*|  |0|  |#|  |D| P5.0
-          P3.1 P3.5 P1.1 P5.4
+          P3.0 P2.5 P4.4 P4.7
     
     MSP Errors:
         P1.3 Always High
@@ -63,7 +64,9 @@
 #define KEY_D 0x088
 
 #define Port1 GPIO_PORT_P1  
+#define Port2 GPIO_PORT_P2
 #define Port3 GPIO_PORT_P3  
+#define Port4 GPIO_PORT_P4
 #define Port5 GPIO_PORT_P5 
 
 #define P0 GPIO_PIN0
@@ -72,6 +75,8 @@
 #define P3 GPIO_PIN3
 #define P4 GPIO_PIN4
 #define P5 GPIO_PIN5
+#define P6 GPIO_PIN6
+#define P7 GPIO_PIN7
 
 void CheckCol();
 void CheckRow();
@@ -79,15 +84,17 @@ void PatternAUpdate();
 void PatternBUpdate();
 void PatternCUpdate();
 void PatternDUpdate();
+void PatternErrorUpdate();
 void ButtonResponse();
 void UpdateLED();
 
 uint8_t Button = 0, LED_Out = 0, Pattern = 0;
 uint8_t PatternBCounter = 0;
 uint8_t PatternDCounter = 0;
+uint8_t PatternECounter = 0;
 uint8_t State = 0; // 0 - wait for key, 1-3 - correct button pressed for password, 
-uint8_t LastButton; // tracker of last button pressed
-uint8_t Passcode[] = {KEY_1, KEY_2, KEY_3};
+uint8_t LastButton = 0; // tracker of last button pressed
+uint8_t const Passcode[] = {KEY_1, KEY_2, KEY_3};
 bool CheckFlag = false;
 bool TimerFlag = false;
 int test; // arbitrary test register
@@ -96,62 +103,63 @@ int main(void) {
     // Stop watchdog timer
     WDT_A_hold(WDT_A_BASE);
     //LED Bar Output initalizing as low
-    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN0);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P3, GPIO_PIN0);
-    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN5);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN5);
-    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN4);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN4);
-    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN7);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN7);
-    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN6);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN6);
-    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN0);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN0);
-    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN2);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN2);
-    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
+    GPIO_setAsOutputPin(Port3, P1);
+    GPIO_setOutputLowOnPin(Port3, P1);
+    GPIO_setAsOutputPin(Port3, P5);
+    GPIO_setOutputLowOnPin(Port3, P5);
+    GPIO_setAsOutputPin(Port1, P1);
+    GPIO_setOutputLowOnPin(Port1, P1);
+    GPIO_setAsOutputPin(Port5, P4);
+    GPIO_setOutputLowOnPin(Port5, P4);
+    GPIO_setAsOutputPin(Port5, P0);
+    GPIO_setOutputLowOnPin(Port5, P0);
+    GPIO_setAsOutputPin(Port5, P1);
+    GPIO_setOutputLowOnPin(Port5, P1);
+    GPIO_setAsOutputPin(Port5, P3);
+    GPIO_setOutputLowOnPin(Port5, P3);
+    GPIO_setAsOutputPin(Port1, P4);
+    GPIO_setOutputLowOnPin(Port1, P4);
 
     //Setting keypad ports as inputs with interupt enabled. todo decide which should be the input. rows vs columns. won't work as is
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P5, GPIO_PIN4);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P1, GPIO_PIN1);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P3, GPIO_PIN5);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P3, GPIO_PIN1);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P5, GPIO_PIN3);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P5, GPIO_PIN1);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P5, GPIO_PIN0);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P1, GPIO_PIN4);
-    P5IFG &= ~BIT0;
-    P5IFG &= ~BIT1;
-    P5IFG &= ~BIT3;
-    P5IFG &= ~BIT4;
-    P1IFG &= ~BIT4;
-    P1IFG &= ~BIT1;
-    P3IFG &= ~BIT5;
-    P3IFG &= ~BIT1;
+    GPIO_setAsInputPinWithPullDownResistor(Port2, P0);
+    GPIO_setAsInputPinWithPullDownResistor(Port2, P2);
+    GPIO_setAsInputPinWithPullDownResistor(Port4, P0);
+    GPIO_setAsInputPinWithPullDownResistor(Port4, P6);
+    GPIO_setAsOutputPin(Port4, P7);
+    GPIO_setAsOutputPin(Port4, P4);
+    GPIO_setAsOutputPin(Port2, P5);
+    GPIO_setAsOutputPin(Port3, P0);
+    GPIO_setOutputHighOnPin(Port4, P7);
+    GPIO_setOutputHighOnPin(Port4, P4);
+    GPIO_setOutputHighOnPin(Port2, P5);
+    GPIO_setOutputHighOnPin(Port3, P0);
+    // Clear interrupt flag bits
+    P2IFG &= ~BIT0; 
+    P2IFG &= ~BIT2;
+    P4IFG &= ~BIT0;
+    P4IFG &= ~BIT6;
+    // Rising edge sensitivity
+    P2IES &= ~BIT0;
+    P2IES &= ~BIT2;
+    P4IES &= ~BIT0;
+    P4IES &= ~BIT6;
 
-    P5IES |= BIT0;
-    P5IES |= BIT1;
-    P5IES |= BIT3;
-    P5IES |= BIT4;
-    P1IES |= BIT4;
-    P1IES |= BIT1;
-    P3IES |= BIT5;
-    P3IES |= BIT1;
+    TB0CTL |= TBCLR;
+    TB0CTL |= TBSSEL__ACLK;
+    TB0CTL |= MC__UP;
+    TB0CTL |= ID__2;
+    TB0CCR0 = 32768;
 
     // Disable the GPIO power-on default high-impedance mode
     // to activate previously configured port settings
     PMM_unlockLPM5();
 
-    P5IE |= BIT0;
-    P5IE |= BIT1;
-    P5IE |= BIT3;
-    P5IE |= BIT4;
-    P1IE |= BIT4;
-    P1IE |= BIT1;
-    P3IE |= BIT5;
-    P3IE |= BIT1;
+    // Interrupt enable
+    P2IE |= BIT0;
+    P2IE |= BIT2;
+    P4IE |= BIT0;
+    P4IE |= BIT6;
+    TB0CCTL0 &= ~CCIFG;
 
     __enable_interrupt();
 
@@ -159,37 +167,70 @@ int main(void) {
         if (CheckFlag) {
             CheckCol();
             CheckRow();
+            ButtonResponse();
+            CheckFlag = false;
+            State++;
         }
         switch(State) {
-            case 0: // start waiting for password
-                if (LastButton == Passcode[0])
+            default: // waiting for keypress / Run pattern based on current value 
+                if (TimerFlag) {
+                    switch (Pattern) { 
+                        case 1: PatternAUpdate(); break;
+                        case 2: PatternBUpdate(); break;
+                        case 3: PatternCUpdate(); break;
+                        case 4: PatternDUpdate(); break;
+                        case 5: PatternErrorUpdate(); break;
+                        default: break;
+                    }
+                    TimerFlag = false;
+                }
+                break;
+            case 1: // respond to keypress
+                if (LastButton == Passcode[0]) {
                     State++;
+                    TB0R = 0;
+                    TB0CCTL0 |= CCIE;
+                }
+                else {
+                    State=0;
+                    TB0CCTL0 &= ~CCIE;
+                }
             break;
-            case 1: // first key entered
-                if (LastButton == Passcode[1])
+            case 3: // first key entered
+                if (LastButton == Passcode[1]) {
                     State++;
+                    TB0CCTL0 |= CCIE;
+                }
+                else {
+                    State=0;
+                    TB0CCTL0 &= ~CCIE;
+                }
             break;
-            case 2: // second key entered
-                if (LastButton == Passcode[2])
+            case 5: // second key entered
+                if (LastButton == Passcode[2]) {
                     State++;
+                    TB0CCTL0 |= CCIE;
+                }
+                else {
+                    State=0;
+                    TB0CCTL0 &= ~CCIE;
+                }
             break;
-            case 3: // password entered
+            case 7: // password entered
                 // switch pattern based on last button pressed
                 switch (LastButton) { 
                     case KEY_A: Pattern = 1; break;
-                    case KEY_B: Pattern = 2;  break;
+                    case KEY_B: 
+                        if (Pattern == 2)
+                            PatternBCounter = 0;
+                        Pattern = 2;  
+                        break;
                     case KEY_C: Pattern = 3;  break;
                     case KEY_D: Pattern = 4;  break;
                     default: Pattern = 0; break;
                 }
-                // Run pattern based on current value 
-                switch (Pattern) { 
-                    case 1: PatternAUpdate(); break;
-                    case 2: PatternBUpdate();  break;
-                    case 3: PatternCUpdate();  break;
-                    case 4: PatternDUpdate();  break;
-                    default: break;
-                }
+                TimerFlag = true;
+                State--;
             break;
             
         }
@@ -206,48 +247,49 @@ void delay_init(){
 
 void CheckCol() {
     // Set necessary pins as inputs and outputs for reading columns
-    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN4);
-    GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN3);
-    GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN1);
-    GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN0);
-    GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN4);
-    GPIO_setOutputHighOnPin(GPIO_PORT_P5, GPIO_PIN3);
-    GPIO_setOutputHighOnPin(GPIO_PORT_P5, GPIO_PIN1);
-    GPIO_setOutputHighOnPin(GPIO_PORT_P5, GPIO_PIN0);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P5, GPIO_PIN4);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P1, GPIO_PIN1);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P3, GPIO_PIN5);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P3, GPIO_PIN1);
+    GPIO_setAsOutputPin(Port2, GPIO_PIN0);
+    GPIO_setAsOutputPin(Port2, GPIO_PIN2);
+    GPIO_setAsOutputPin(Port4, GPIO_PIN0);
+    GPIO_setAsOutputPin(Port4, GPIO_PIN6);
+    GPIO_setOutputHighOnPin(Port2, GPIO_PIN0);
+    GPIO_setOutputHighOnPin(Port2, GPIO_PIN2);
+    GPIO_setOutputHighOnPin(Port4, GPIO_PIN0);
+    GPIO_setOutputHighOnPin(Port4, GPIO_PIN6);
+    GPIO_setAsInputPinWithPullDownResistor(Port4, P7);
+    GPIO_setAsInputPinWithPullDownResistor(Port4, P4);
+    GPIO_setAsInputPinWithPullDownResistor(Port2, P5);
+    GPIO_setAsInputPinWithPullDownResistor(Port3, P0);
 
     // Clear button column values
     Button &= 0x0F0;
 
-    Button = ((P5IN & BIT4) == BIT4) ? (Button | BIT3) : (Button & ~BIT3);
-    Button = ((P1IN & BIT1) == BIT1) ? (Button | BIT2) : (Button & ~BIT2);
-    Button = ((P3IN & BIT5) == BIT5) ? (Button | BIT1) : (Button & ~BIT1);
-    Button = ((P3IN & BIT1) == BIT1) ? (Button | BIT0) : (Button & ~BIT0);
-
-    test = P3IN;
-    test &= BIT1;
-    if (test == BIT1)
-        Button |= BIT0;
+    Button = ((P4IN & BIT7) == BIT7) ? (Button | BIT3) : (Button & ~BIT3);
+    Button = ((P4IN & BIT4) == BIT4) ? (Button | BIT2) : (Button & ~BIT2);
+    Button = ((P2IN & BIT5) == BIT5) ? (Button | BIT1) : (Button & ~BIT1);
+    Button = ((P3IN & BIT0) == BIT0) ? (Button | BIT0) : (Button & ~BIT0);
 
 }
 
 void CheckRow() {
     // Set necessary pins as inputs and outputs for reading columns
-    GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN4);
-    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN1);
-    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN5);
-    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN1);
-    GPIO_setOutputHighOnPin(GPIO_PORT_P5, GPIO_PIN4);
-    GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN1);
-    GPIO_setOutputHighOnPin(GPIO_PORT_P3, GPIO_PIN5);
-    GPIO_setOutputHighOnPin(GPIO_PORT_P3, GPIO_PIN1);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P1, GPIO_PIN4);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P5, GPIO_PIN3);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P5, GPIO_PIN1);
-    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P5, GPIO_PIN0);
+    GPIO_setAsOutputPin(Port4, P7);
+    GPIO_setAsOutputPin(Port4, P4);
+    GPIO_setAsOutputPin(Port2, P5);
+    GPIO_setAsOutputPin(Port3, P0);
+    GPIO_setOutputHighOnPin(Port4, P7);
+    GPIO_setOutputHighOnPin(Port4, P4);
+    GPIO_setOutputHighOnPin(Port2, P5);
+    GPIO_setOutputHighOnPin(Port3, P0);
+    GPIO_setAsInputPinWithPullDownResistor(Port2, P0);
+    GPIO_setAsInputPinWithPullDownResistor(Port2, P2);
+    GPIO_setAsInputPinWithPullDownResistor(Port4, P0);
+    GPIO_setAsInputPinWithPullDownResistor(Port4, P6);
+
+    Button &= 0x00F;
+    Button = ((P2IN & BIT0) == BIT0) ? (Button | BIT7) : (Button & ~BIT7);
+    Button = ((P2IN & BIT2) == BIT2) ? (Button | BIT6) : (Button & ~BIT6);
+    Button = ((P4IN & BIT0) == BIT0) ? (Button | BIT5) : (Button & ~BIT5);
+    Button = ((P4IN & BIT6) == BIT6) ? (Button | BIT4) : (Button & ~BIT4);
 }
 
 void ButtonResponse() {
@@ -275,6 +317,14 @@ void PatternAUpdate() {
 }
 
 void PatternBUpdate() {
+    TB0CTL |= TBCLR;
+    TB0CTL |= TBSSEL__ACLK;
+    TB0CTL |= MC__UP;
+    TB0CTL |= ID__1;
+    TB0CCR0 = 16384;
+    TB0CCTL0 |= CCIE;
+    TB0CCTL0 &= ~CCIFG;
+    TB0R = 0;
     // Count and reset on rollover
     PatternBCounter += 1;
     LED_Out = PatternBCounter;
@@ -292,46 +342,63 @@ void PatternDUpdate() {
         case 0: LED_Out = 0x018; break;
         case 1: LED_Out = 0x024; break;
         case 2: LED_Out = 0x042; break;
-        case 3: LED_Out = 0x018; break;
+        case 3: LED_Out = 0x081; break;
         case 4: LED_Out = 0x042; break;
         case 5: LED_Out = 0x024; break;
     }
 }
 
+void PatternErrorUpdate() {
+    PatternECounter += 1;
+    LED_Out = 0x000; 
+    if (PatternECounter % 2 == 0) {
+        LED_Out = 0x0FF;
+    }
+    if (PatternECounter == 7) {
+        PatternECounter = 0;
+        Pattern = 0;
+    }
+
+
+}
+
 void UpdateLED() {
     // Move LED_Out to led ports
-    P3OUT = ((LED_Out & BIT0) == BIT0) ? (P3OUT | BIT0) : (P3OUT & ~BIT0);
-    P2OUT = ((LED_Out & BIT1) == BIT1) ? (P2OUT | BIT5) : (P2OUT & ~BIT5);
-    P4OUT = ((LED_Out & BIT2) == BIT2) ? (P4OUT | BIT4) : (P4OUT & ~BIT4);
-    P4OUT = ((LED_Out & BIT3) == BIT3) ? (P4OUT | BIT7) : (P4OUT & ~BIT7);
-    P4OUT = ((LED_Out & BIT4) == BIT4) ? (P4OUT | BIT6) : (P4OUT & ~BIT6);
-    P4OUT = ((LED_Out & BIT5) == BIT5) ? (P4OUT | BIT0) : (P4OUT & ~BIT0);
-    P2OUT = ((LED_Out & BIT6) == BIT6) ? (P2OUT | BIT2) : (P2OUT & ~BIT2);
-    P2OUT = ((LED_Out & BIT7) == BIT7) ? (P2OUT | BIT0) : (P2OUT & ~BIT0);    
+    P1OUT = ((LED_Out & BIT0) == BIT0) ? (P1OUT | BIT4) : (P1OUT & ~BIT4);
+    P5OUT = ((LED_Out & BIT1) == BIT1) ? (P5OUT | BIT3) : (P5OUT & ~BIT3);
+    P5OUT = ((LED_Out & BIT2) == BIT2) ? (P5OUT | BIT1) : (P5OUT & ~BIT1);
+    P5OUT = ((LED_Out & BIT3) == BIT3) ? (P5OUT | BIT0) : (P5OUT & ~BIT0);
+    P5OUT = ((LED_Out & BIT4) == BIT4) ? (P5OUT | BIT4) : (P5OUT & ~BIT4);
+    P1OUT = ((LED_Out & BIT5) == BIT5) ? (P1OUT | BIT1) : (P1OUT & ~BIT1);
+    P3OUT = ((LED_Out & BIT6) == BIT6) ? (P3OUT | BIT5) : (P3OUT & ~BIT5);
+    P3OUT = ((LED_Out & BIT7) == BIT7) ? (P3OUT | BIT1) : (P3OUT & ~BIT1);    
 }
 
 //-- Interrupt Service Routines --------------------------
-#pragma vector = PORT1_VECTOR
-__interrupt void ISR_P1_Button_Pressed(void) {
+#pragma vector = PORT2_VECTOR
+__interrupt void ISR_P2_Button_Pressed(void) {
     CheckFlag = true;
-    P1IV &= ~BIT4;  // Clear CCR0 Flag
+    P2IFG &= ~BIT0;  // Clear CCR0 Flag
+    P2IFG &= ~BIT2;  // Clear CCR0 Flag
 }
 //-- End ISR_P1_Button_Pressed ----------------
 
-#pragma vector = PORT5_VECTOR
-__interrupt void ISR_P5_Button_Pressed(void) {
+#pragma vector = PORT4_VECTOR
+__interrupt void ISR_P4_Button_Pressed(void) {
     CheckFlag = true;
-    P5IV &= ~BIT0; // Clear Flag
-    P5IV &= ~BIT1;
-    P5IV &= ~BIT3;
+    P4IV &= ~BIT0; // Clear Flag
+    P4IV &= ~BIT6;
 }
 //-- End ISR_P5_Button_Pressed ----------------
 
 #pragma vector=TIMER0_B0_VECTOR
 __interrupt void Timer_B_ISR(void){
-    if (State < 3)
-        State = 0;
-    else 
+    if (State < 6) {
+     //   State = 0;
+        Pattern = 5;
+        TimerFlag = true;
+        TB0CCTL0 &= ~CCIE;
+    } else 
         TimerFlag = true;  
 
     TB0CCTL0 &= ~CCIFG; 
