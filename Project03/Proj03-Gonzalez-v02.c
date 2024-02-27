2b584c58161d7757f819a4603bf4bb1519895f00
Lance G <lancedgonzalez@gmail.com>
2024-02-26 20:57:29 -0700

    v02: intialized correctly

--- Project03/Proj03-Gonzalez-v02.c
+++ Project03/Proj03-Gonzalez-v02.c
@@ -42,6 +42,7 @@
 #define Port3 GPIO_PORT_P3  
 #define Port4 GPIO_PORT_P4
 #define Port5 GPIO_PORT_P5 
+#define Port6 GPIO_PORT_P6
 
 #define Pin0 GPIO_PIN0
 #define Pin1 GPIO_PIN1
@@ -57,6 +58,7 @@ void Initialization();
 void SetCollumn();
 void SetRows(); 
 void LedLow(); 
+void LedLocked();
 
 
 int main(void) {
@@ -69,6 +71,8 @@ int main(void) {
     // Disable the GPIO power-on default high-impedance 
     PMM_unlockLPM5();
 
+    Initialization();
+
     while(1){}
 
 }
@@ -76,7 +80,7 @@ int main(void) {
 //-----Subroutines-----------------------------------------------------------------------------------
 //-Initalization: Main initialization for startup----------------------------------------------------
 void Initialization(){
-    LedLow(); 
+    LedLocked(); 
     SetCollumn(); 
 }//--END Initialization-----------------------------------------------------------------------------
 
@@ -136,8 +140,8 @@ void LedLow(){
     GPIO_setOutputLowOnPin(Port1, Pin1);
     GPIO_setAsOutputPin(Port5, Pin4);
     GPIO_setOutputLowOnPin(Port5, Pin4);
-    GPIO_setAsOutputPin(Port5, P0);
-    GPIO_setOutputLowOnPin(Port5, P0);
+    GPIO_setAsOutputPin(Port5, Pin0);
+    GPIO_setOutputLowOnPin(Port5, Pin0);
     GPIO_setAsOutputPin(Port5, Pin1);
     GPIO_setOutputLowOnPin(Port5, Pin1);
     GPIO_setAsOutputPin(Port5, Pin3);
@@ -146,3 +150,25 @@ void LedLow(){
     GPIO_setOutputLowOnPin(Port1, Pin4);
 }//--END LedLow-------------------------------------------------------------------------------------
 
+//-LED Locked: Turns on all LEDs indicating lockout-------------------------------------------------
+void LedLocked(){
+    //Configure outputs on LED PINS
+    GPIO_setAsOutputPin(Port2, Pin2);
+    GPIO_setAsOutputPin(Port4, Pin0);
+    GPIO_setAsOutputPin(Port6, Pin3);
+    GPIO_setAsOutputPin(Port4, Pin6);
+    GPIO_setAsOutputPin(Port4, Pin7);
+    GPIO_setAsOutputPin(Port4, Pin4);
+    GPIO_setAsOutputPin(Port2, Pin5);
+    GPIO_setAsOutputPin(Port3, Pin0);
+
+    //Set High on Collumns
+    GPIO_setOutputHighOnPin(Port2, Pin2);
+    GPIO_setOutputHighOnPin(Port4, Pin0);
+    GPIO_setOutputHighOnPin(Port6, Pin3);
+    GPIO_setOutputHighOnPin(Port4, Pin6);
+    GPIO_setOutputHighOnPin(Port4, Pin7);
+    GPIO_setOutputHighOnPin(Port4, Pin4);
+    GPIO_setOutputHighOnPin(Port2, Pin5);
+    GPIO_setOutputHighOnPin(Port3, Pin0);
+}//--END LedLocked---------------------------------------------------------------------------------
