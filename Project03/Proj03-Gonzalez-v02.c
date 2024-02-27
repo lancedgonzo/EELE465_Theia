1d2876da7c2e289779c98956f9f331d8684607e9
Lance G <lancedgonzalez@gmail.com>
2024-02-26 20:14:46 -0700

    v02: added set row and collumn functions

--- Project03/Proj03-Gonzalez-v02.c
+++ Project03/Proj03-Gonzalez-v02.c
@@ -40,31 +40,26 @@
 #include <driverlib.h>
 #include <stdint.h>
 
+//Macro Definitions for ease of coding -----------------------------------------------------------------
+#define Port1 GPIO_PORT_P1  
+#define Port2 GPIO_PORT_P2
+#define Port3 GPIO_PORT_P3  
+#define Port4 GPIO_PORT_P4
+#define Port5 GPIO_PORT_P5 
 
-#define  GPIO_PORT_P1 Port1 
-#define  GPIO_PORT_P3 Port3 
-#define  GPIO_PORT_P5 Port5
+#define Pin0 GPIO_PIN0
+#define Pin1 GPIO_PIN1
+#define Pin2 GPIO_PIN2
+#define Pin3 GPIO_PIN3
+#define Pin4 GPIO_PIN4
+#define Pin5 GPIO_PIN5
+#define Pin6 GPIO_PIN6
+#define Pin7 GPIO_PIN7
 
-#define GPIO_PIN_PIN0 Pin0 
-#define GPIO_PIN_PIN1 Pin1
-#define GPIO_PIN_PIN2 Pin2
-#define GPIO_PIN_PIN3 Pin3   
-#define GPIO_PIN_PIN4 Pin4
-#define GPIO_PIN_PIN5 Pin5 
+//Function Declarations ----------------------------------------------------------------------------------------
+void SetCollumn();
+void SetRows(); 
 
-uint8_t pinConfig_G1[][2] = {
-    {Port3, Pin1}, //P3.1
-    {Port3, Pin5}, //P3.5
-    {Port1, Pin1}, //P1.1
-    {Port5, Pin4}, //P5.4 
-};
-
-uint8_t pinConfig_G2[][2] = {
-    {Port1, Pin4}, //P1.4
-    {Port5, Pin3}, //P5.3
-    {Port5, Pin1}, //P5.1
-    (Port5, Pin0)  //P5.0
-};
 
 int main(void) {
 
@@ -76,25 +71,55 @@ int main(void) {
     // Disable the GPIO power-on default high-impedance 
     PMM_unlockLPM5();
 
-
+    SetRows(); 
+    while(1){}
 
 }
 
 //-----Subroutines-----------------------------------------------------------------------------------
 //-Collumns: Sets Collumn pins as OUTPUT-------------------------------------------------------------
-void Collumns(){
-    for (int i = 0; i < sizeof(pinConfig_G1) / sizeof(pinConfig_G1[0]); i++){
-        pinMode(pinConfig_G1[i][0], OUTPUT)
-    }
-}//--END Collumns-------------------------------------------------------------------------------------
+void SetCollumn(){
+
+    //Set P0-3 on keyboard as outputs
+    GPIO_setAsOutputPin(Port3, Pin6);
+    GPIO_setAsOutputPin(Port4, Pin5);
+    GPIO_setAsOutputPin(Port1, Pin1);
+    GPIO_setAsOutputPin(Port3, Pin4);
+
+    //Set P4-7 on keyboard as inputs with pull down
+    GPIO_setAsInputPinWithPullDownResistor(Port3, Pin5);
+    GPIO_setAsInputPinWithPullDownResistor(Port1, Pin3);
+    GPIO_setAsInputPinWithPullDownResistor(Port3, Pin1);
+    GPIO_setAsInputPinWithPullDownResistor(Port1, Pin2);
+
+    //Set P0-3 on keyboard as HIGH
+    GPIO_setOutputHighOnPin(Port3, Pin6);
+    GPIO_setOutputHighOnPin(Port4, Pin5);
+    GPIO_setOutputHighOnPin(Port1, Pin1);
+    GPIO_setOutputHighOnPin(Port3, Pin4);
+
+}//--END SetCollumns-------------------------------------------------------------------------------------
 
 //-Rows: Sets row pins as OUTPUT---------------------------------------------------------------------
-void Rows(){
-    for (int i = 0; i < sizeof(pinConfig_G2) / sizeof(pinConfig_G2[0]); i++){
-        pinMode(pinConfig_G2[i][0], OUTPUT)
-        pinMode(pinConfig_G1[i][0], INPUT)
-    }
+  void SetRows(){
+  
+  //Configure outputs on Rows
+    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN5);
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN3);
+    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN1);
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN2);
+
+    //Configure inputs on Collumns
+    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P3, GPIO_PIN6);
+    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P4, GPIO_PIN5);
+    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P1, GPIO_PIN1);
+    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P3, GPIO_PIN4);
+
+    //Set High on ROWS
+    GPIO_setOutputHighOnPin(GPIO_PORT_P3, GPIO_PIN5);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN3);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P3, GPIO_PIN1);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN2);
 }//--END Rows----------------------------------------------------------------------------------------
 
 
-}
