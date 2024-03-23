f1345226d3134601a39a60ac2d0013a4abf922aa
Myrsine <grantmkirkland@gmail.com>
2024-03-22 19:09:04 -0600

    added final presentation material

---
+++ Project04_Master/Proj04-LCD-Slave_Gonzalez-v01.c
@@ -0,0 +1,182 @@
+/*-----------------------------------------------------------------------------
+    EELE465
+	Written by: Lance Gonzalez
+	Project 04 - Mar 03 2024
+
+	Summary:
+
+	Version Summary:
+        v01: I2C as slave setup
+        v02: LCD communication
+
+    Ports Map: 
+        2310 LCD Slave
+            P1.2 - I2C SDA  (PIN 16)
+            P1.3 - I2C SCL  (PIN 15)
+
+            P1.4 - DB0      (PIN 14)
+            P1.5 - DB1      (PIN 13)
+            P1.6 - DB2      (PIN 11)
+            P1.7 - DB3      (PIN 10)
+
+            P2.0 - E = 6    (PIN 9)
+            P2.6 - RS = 4   (PIN 8)
+            P2.7 - R/W = 5  (PIN 7) 
+
+
+	Important Variables/Registers:
+	
+
+    MSP Errors:
+
+	Todo:
+		
+-----------------------------------------------------------------------------*/
+
+//----- Library/Header Includes -----------------------------------------------
+#include <msp430.h>
+#include "msp430fr2310.h"
+
+// #include <stdint.h>
+//-----------------------------------------------------------------------------
+
+//----- MACRO definitions and Global Variables --------------------------------
+//Port2
+#define LCD_RS BIT6  // Register select pin 
+#define LCD_EN BIT0  // Enable pin
+#define LCD_RW BIT7  // Read/Write pin
+
+//Port1
+#define LCD_D4 BIT4  // Data line 4
+#define LCD_D5 BIT5  // Data line 5
+#define LCD_D6 BIT6  // Data line 6
+#define LCD_D7 BIT7  // Data line 7
+
+const char clearDisplay =      0b00000001;
+const char returnHome =        0b00000010;
+const char entryMode =         0b00000110;
+const char displayCursorOn =   0b00001111; //cursor on and blink
+const char displayOn =         0b00001100; //cursor off and no blink
+const char displayOff =        0b00001000;
+const char fourBitMode  =      0b00101100; //function set NF
+const char setDDRAM  =         0b10000000; // | this with whatever addy then driveChar
+
+//-----------------------------------------------------------------------------
+
+//----- Function Declarations -------------------------------------------------
+void latch(); 
+void LCD_init();
+void LCD_command(char); 
+void LCD_data(char);  
+void delay(); 
+I2C_slaveRx(); 
+
+//-----------------------------------------------------------------------------
+
+int main(void) {
+
+    // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+    I2C_slaveRx(); 
+    LCD_init(); // Initialize LCD
+    PM5CTL0 &= ~LOCKLPM5;
+
+    while (1){}
+
+    return 0;
+}
+
+
+//----- Subroutines & Function Definitions ------------------------------------
+//- I2C_slaveRx: Initalizes the controller as a slave with address 0x00
+void I2C_slaveRx(void){
+    UCB0CTLW0 |= UCSWRST;        //eUSCI-B0 software reset
+    UCB0CTLW0 |= UCMODE_3;      //I2C slave mode 
+    UCB0CTLW0 &= ~UCMST;           //again?
+    UCB0CTLW0 &= ~UCTR;         //clears transmit mode select bit
+    UCB0I2COA0 = 0X046;        //Slave address
+    UCB0I2COA0 |= UCOAEN;       //Enables I2C own address
+    UCB0CTLW1 &= ~UCASTP1;      //clear auto stop bit
+    UCB0CTLW1 &= ~UCASTP0;
+
+    //Port Setup
+    P1SEL1 &= ~BIT3; 
+    P1SEL1 &= ~BIT2; 
+
+    P1SEL0 |= BIT3; 
+    P1SEL0 |= BIT2; 
+
+    UCB0CTLW1 |= UCASTP_2;      //enable stop bit mode 2
+    UCB0CTL1 &= ~UCSWRST;       //eUSCI-B0 in operational state 
+
+    UCB0IE |= UCRXIE0;  //Enable Tx and Rx interrupt 
+    __enable_interrupt(); 
+}//--END I2C_slaveRx-----------------------------------------------------------
+
+void LCD_init(){
+    //Port Initialization
+    P2DIR |= (LCD_RS | LCD_EN | LCD_RW);  
+    P1DIR |= (LCD_D4 | LCD_D5 | LCD_D6 | LCD_D7); // Set LCD control pins as output
+
+    P2OUT &= ~(LCD_RS | LCD_EN | LCD_RW);
+    P1OUT &= ~(LCD_D4 | LCD_D5 | LCD_D6 | LCD_D7); // Initialize control pins to low
+
+    delay(30);          // Wait >15ms after VDD rises to 4.5V
+    // LCD_command(0b00110011);
+    // LCD_command(0b00110010);
+
+    // LCD_command(0x00 | setDDRAM);
+
+    LCD_command(fourBitMode); 
+    LCD_command(displayOff); 
+    LCD_command(clearDisplay);
+    LCD_command(entryMode);
+    LCD_command(displayOn); 
+
+    LCD_data('H');
+    LCD_data('Y')
+    // LCD_command(0x40 | setDDRAM);
+}
+
+//-LCD Latch: -----------------------------------------------------------------
+void latch(){
+    P2OUT |= LCD_EN;      // set enable
+    delay(50); 
+    P2OUT &= ~LCD_EN;    // clear enable
+    delay(50);
+}//--END LCD latch ------------------------------------------------------------
+
+void LCD_command(char command){
+    P2OUT &= ~LCD_RS; // Set RS low for command
+    P1OUT = (command & 0xF0);
+    latch();
+    P1OUT = ((command & 0x0F) << 4);
+    latch(); 
+}
+
+void LCD_data(char data){
+    P2OUT |= LCD_RS;  // Set RS high for data
+    P1OUT = (data & 0xF0);
+    latch(); 
+    P1OUT = ((data & 0x0F) << 4);
+    latch(); 
+}
+
+
+//-Delay: --------------------------------------------------------------------
+void delay(unsigned int ms){
+    unsigned int i;
+    for (i = 0; i < ms; i++){
+        __delay_cycles(1000); // Assuming 1MHz clock
+    }
+}//--END Delay --------------------------------------------------------------
+
+
+// // Interrupt service routine for I2C
+// #pragma vector = EUSCI_B0_VECTOR 
+// __interrupt void EUSCI_B0_I2C_ISR(void) {
+//         LCD_data(UCB0RXBUF);
+// }
+//-----------------------------------------------------------------------------
+
--- Project04_Master/Project04_MasterMain.c
+++ Project04_Master/Project04_MasterMain.c
@@ -2,7 +2,7 @@
     EELE465
 	Written by: Lance Gonzalez, Grant Kirkland
     Working: 
-	Project 03 - Feb 15 2024
+	Project 04 - Feb 15 2024
 
 	Summary:
 
@@ -11,6 +11,22 @@
         v02: Working LED Output, defined keypad macros, working column readout
         v03: Working keypad password. Working Pattern B. 
         v04: Added lock button. Working Pattern D. Working Keypad reset
+
+    MSP430FR2355:
+         4 - SBWTCK - SBWTCK
+         5 - SBWTDIO - SBWTDIO
+         6 - DVCC - 3v3
+         7 - DVSS - GND
+        12 - P4.7 - SCL
+        13 - P4.6 - SDA
+        25 - P4.1 - P7 Row4
+        26 - P4.0 - P6 Row3
+        27 - P2.3 - P5 Row2
+        28 - P2.2 - P4 Row1
+        29 - P2.1 - P3 Col4
+        30 - P2.0 - P2 Col3
+        31 - P1.7 - P1 Col2
+        32 - P1.6 - P0 Col1
 
     Ports Map: 
         P3.1, 3.5, 1.1, 5.4, 1.4, 5.3, 5.1, 5.0 - Keyboard P1-8
@@ -25,11 +41,11 @@
 
     Keypad: (Pins L->R)
             P0   P1   P2   P3
-        P4 |1|  |2|  |3|  |A| P4.6
+        P4 |1|  |2|  |3|  |A| P3.7
         P5 |4|  |5|  |6|  |B| P4.0
         P6 |7|  |8|  |9|  |C| P2.2
         P7 |*|  |0|  |#|  |D| P2.0
-          P3.0 P2.5 P4.4 P4.7
+          P3.0 P2.5 P4.4 P2.4
     
     MSP Errors:
         P1.3 Always High
@@ -40,17 +56,25 @@
 -----------------------------------------------------------------------------------------------------------------------*/
 
 
-#include "gpio.h"
+#include <stdint.h>
 #include "msp430fr2355.h"
 #include "stdbool.h"
 #include "sys/_stdint.h"
 #include <msp430.h>
 #include <driverlib.h>
-#include <stdint.h>
+#include "driverlib/gpio.c"
 
- #define LED_Address 0x048
- #define LCD_Address 0x046
+#define LED_Address 0x013
+#define LCD_Address 0x046
 
+#define KEYPAD_COL1(r) (r == 0) ? 1 : 0x0040 // 1.6
+#define KEYPAD_COL2(r) (r == 0) ? 1 : 0x0080 // 1.7
+#define KEYPAD_COL3(r) (r == 0) ? 2 : 0x0001 // 2.0
+#define KEYPAD_COL4(r) (r == 0) ? 2 : 0x0002 // 2.1
+#define KEYPAD_ROW1(r) (r == 0) ? 2 : 0x0004 // 2.2
+#define KEYPAD_ROW2(r) (r == 0) ? 2 : 0x0008 // 2.3
+#define KEYPAD_ROW3(r) (r == 0) ? 4 : 0x0001 // 4.0
+#define KEYPAD_ROW4(r) (r == 0) ? 4 : 0x0002 // 4.1
 
 // Defining row and column input as keys
 #define KEY_1 0x011 // 17
@@ -70,21 +94,6 @@
 #define KEY_C 0x048 // 72
 #define KEY_D 0x088 // 136
 
-// Making ports and pins easier to reference
-#define Port1 1
-#define Port2 2
-#define Port3 3
-#define Port4 4
-#define Port5 5
-
-#define P0 (0x0001)
-#define P1 (0x0002)
-#define P2 (0x0004)
-#define P3 (0x0008)
-#define P4 (0x0010)
-#define P5 (0x0020)
-#define P6 (0x0040)
-#define P7 (0x0080)
 
 //Function Declarations ------------------------------------------------------
 void ColInput();
@@ -116,15 +125,18 @@ int main(void) {
     RowInput();
 
     // Clear interrupt flag bits
-    P2IFG &= ~BIT0;
-    P2IFG &= ~BIT2;
-    P4IFG &= ~BIT0;
-    P3IFG &= ~BIT7;
+    GPIO_clearInterrupt(KEYPAD_ROW1(0), KEYPAD_ROW1(1));
+    GPIO_clearInterrupt(KEYPAD_ROW2(0), KEYPAD_ROW2(1));
+    GPIO_clearInterrupt(KEYPAD_ROW3(0), KEYPAD_ROW3(1));
+    GPIO_clearInterrupt(KEYPAD_ROW4(0), KEYPAD_ROW4(1));
     // Rising edge sensitivity
-    P2IES &= ~BIT0;
-    P2IES &= ~BIT2;
-    P4IES &= ~BIT0;
-    P3IES &= ~BIT7;
+    GPIO_selectInterruptEdge(KEYPAD_ROW1(0), KEYPAD_ROW1(1), GPIO_LOW_TO_HIGH_TRANSITION);
+    GPIO_selectInterruptEdge(KEYPAD_ROW2(0), KEYPAD_ROW2(1), GPIO_LOW_TO_HIGH_TRANSITION);
+    GPIO_selectInterruptEdge(KEYPAD_ROW3(0), KEYPAD_ROW3(1), GPIO_LOW_TO_HIGH_TRANSITION);
+    GPIO_selectInterruptEdge(KEYPAD_ROW4(0), KEYPAD_ROW4(1), GPIO_LOW_TO_HIGH_TRANSITION);
+
+    GPIO_setAsOutputPin(1, 0x04);
+    P1OUT |= BIT3;
 
     UCB1CTLW0 |= UCSWRST; // Put UCB1CTLW0 into software reset
     UCB1CTLW0 |= UCSSEL_3; // Select mode 3
@@ -136,9 +148,9 @@ int main(void) {
 
     UCB1CTLW1 |= UCASTP_2; // Autostop enabled
     // P1.2 SDA
-    P1SEL1 &= ~(BIT2+BIT3);
+//    P1SEL1 &= ~(BIT2+BIT3);
 //    P1SEL1 |= (BIT2+BIT3);
-    P1SEL0 |= (BIT2+BIT3);
+//    P1SEL0 |= (BIT2+BIT3);
     //P1SEL0 &= ~(BIT2+BIT3);
     //----- P4.6 and P4.7 for I2C ---
     P4SEL1 &= ~BIT7;
@@ -156,17 +168,19 @@ int main(void) {
     UCB1IE |= UCRXIE0; // Enable RX interrupt
 
     // Interrupt enable
-    P2IE |= BIT0;
-    P2IE |= BIT2;
-    P4IE |= BIT0;
-    P3IE |= BIT7;
+    GPIO_enableInterrupt(KEYPAD_ROW1(0), KEYPAD_ROW1(1));
+    GPIO_enableInterrupt(KEYPAD_ROW2(0), KEYPAD_ROW2(1));
+    GPIO_enableInterrupt(KEYPAD_ROW3(0), KEYPAD_ROW3(1));
+    GPIO_enableInterrupt(KEYPAD_ROW4(0), KEYPAD_ROW4(1));
 
     __enable_interrupt();
 
     while(1) {
+        P1OUT |= BIT3;
         if (CheckFlag) {
             CheckButton();
             State++;
+            P1OUT &= ~BIT3;
         }
         switch(State) {
             case 1: // respond to keypress
@@ -234,7 +248,7 @@ void TransmitButton() {
             //...equal to the slave address
             UCB1CTLW0 |= UCTR; // Put into transmit mode
             UCB1CTLW0 |= UCTXSTT; // Generate the start condition
-            for (i = 65000; i > 0; i--) {/* Delay */}
+            for (i = 40; i > 0; i--) {/* Delay */}
         default:
             UCB1TBCNT = 1;
             UCB1I2CSA = LCD_Address; // Set the slave address in the module
@@ -249,31 +263,23 @@ void TransmitButton() {
 //-ColumnInput: Sets row pins as OUTPUT---------------------------------------------------------------------
 void ColumnInput(){
 
-  //Configure outputs on Rows
-    P2DIR |= P0;
-    P2DIR |= P2;
-    P4DIR |= P0;
-    P3DIR |= P7;
+    //Configure outputs on Rows
+    GPIO_setAsOutputPin(KEYPAD_ROW1(0), KEYPAD_ROW1(1));
+    GPIO_setAsOutputPin(KEYPAD_ROW2(0), KEYPAD_ROW2(1));
+    GPIO_setAsOutputPin(KEYPAD_ROW3(0), KEYPAD_ROW3(1));
+    GPIO_setAsOutputPin(KEYPAD_ROW4(0), KEYPAD_ROW4(1));
 
     //Configure inputs on Collumns
-    P2DIR &= ~P4;
-    P4DIR &= ~P4;
-    P2DIR &= ~P5;
-    P3DIR &= ~P0;
-    P2REN |= P4;
-    P4REN |= P4;
-    P2REN |= P5;
-    P3REN |= P0;
-    P2OUT &= ~P4;
-    P4OUT &= ~P4;
-    P2OUT &= ~P5;
-    P3OUT &= ~P0;
+    GPIO_setAsInputPinWithPullDownResistor(KEYPAD_COL1(0), KEYPAD_COL1(1));
+    GPIO_setAsInputPinWithPullDownResistor(KEYPAD_COL2(0), KEYPAD_COL2(1));
+    GPIO_setAsInputPinWithPullDownResistor(KEYPAD_COL3(0), KEYPAD_COL3(1));
+    GPIO_setAsInputPinWithPullDownResistor(KEYPAD_COL4(0), KEYPAD_COL4(1));
 
     //Set High on ROWS
-    P2OUT |= P0;
-    P2OUT |= P2;
-    P4OUT |= P0;
-    P3OUT |= P7;
+    GPIO_setOutputHighOnPin(KEYPAD_ROW1(0), KEYPAD_ROW1(1));
+    GPIO_setOutputHighOnPin(KEYPAD_ROW2(0), KEYPAD_ROW2(1));
+    GPIO_setOutputHighOnPin(KEYPAD_ROW3(0), KEYPAD_ROW3(1));
+    GPIO_setOutputHighOnPin(KEYPAD_ROW4(0), KEYPAD_ROW4(1));
 
 }//--END ColumnInput----------------------------------------------------------------------------------------
 
@@ -284,42 +290,33 @@ void CheckCol() {
 
     // Clear button column values, then toggle bits corresponding to column
     Button &= 0x0F0;
-    Button = ((P2IN & BIT4) == BIT4) ? (Button | BIT3) : (Button & ~BIT3);
-    Button = ((P4IN & BIT4) == BIT4) ? (Button | BIT2) : (Button & ~BIT2);
-    Button = ((P2IN & BIT5) == BIT5) ? (Button | BIT1) : (Button & ~BIT1);
-    Button = ((P3IN & BIT0) == BIT0) ? (Button | BIT0) : (Button & ~BIT0);
+    Button = ((P1IN & (KEYPAD_COL1(1))) == (KEYPAD_COL1(1))) ? (Button | BIT0) : (Button & ~BIT0);
+    Button = ((P1IN & (KEYPAD_COL2(1))) == (KEYPAD_COL2(1))) ? (Button | BIT1) : (Button & ~BIT1);
+    Button = ((P2IN & (KEYPAD_COL3(1))) == (KEYPAD_COL3(1))) ? (Button | BIT2) : (Button & ~BIT2);
+    Button = ((P2IN & (KEYPAD_COL4(1))) == (KEYPAD_COL4(1))) ? (Button | BIT3) : (Button & ~BIT3);
 
 }//--END CheckCol-------------------------------------------------------------------------------------
 
+
 //-Collumns: Sets Collumn pins as OUTPUT-------------------------------------------------------------
 void RowInput(){
-
     //Set P0-3 on keyboard as outputs
-    P2DIR |= P4;
-    P4DIR |= P4;
-    P2DIR |= P5;
-    P3DIR |= P0;
+    GPIO_setAsOutputPin(KEYPAD_COL1(0), KEYPAD_COL1(1));
+    GPIO_setAsOutputPin(KEYPAD_COL2(0), KEYPAD_COL2(1));
+    GPIO_setAsOutputPin(KEYPAD_COL3(0), KEYPAD_COL3(1));
+    GPIO_setAsOutputPin(KEYPAD_COL4(0), KEYPAD_COL4(1));
 
     //Set P4-7 on keyboard as inputs with pull down
-    P2DIR &= ~P0;
-    P2DIR &= ~P2;
-    P4DIR &= ~P0;
-    P3DIR &= ~P7;
-    P2REN |= P0;
-    P2REN |= P2;
-    P4REN |= P0;
-    P3REN |= P7;
-    P2OUT &= ~P0;
-    P2OUT &= ~P2;
-    P4OUT &= ~P0;
-    P3OUT &= ~P7;
-
+    GPIO_setAsInputPinWithPullDownResistor(KEYPAD_ROW1(0), KEYPAD_ROW1(1));
+    GPIO_setAsInputPinWithPullDownResistor(KEYPAD_ROW2(0), KEYPAD_ROW2(1));
+    GPIO_setAsInputPinWithPullDownResistor(KEYPAD_ROW3(0), KEYPAD_ROW3(1));
+    GPIO_setAsInputPinWithPullDownResistor(KEYPAD_ROW4(0), KEYPAD_ROW4(1));
 
     //Set P0-3 on keyboard as HIGH
-    P2OUT |= P4;
-    P4OUT |= P4;
-    P2OUT |= P5;
-    P3OUT |= P0;
+    GPIO_setOutputHighOnPin(KEYPAD_COL1(0), KEYPAD_COL1(1));
+    GPIO_setOutputHighOnPin(KEYPAD_COL2(0), KEYPAD_COL2(1));
+    GPIO_setOutputHighOnPin(KEYPAD_COL3(0), KEYPAD_COL3(1));
+    GPIO_setOutputHighOnPin(KEYPAD_COL4(0), KEYPAD_COL4(1));
 
 }//--END RowInputs---------------------------------------------------------------------------------
 
@@ -330,10 +327,10 @@ void CheckRow() {
 
     // Clear row portion of button, then toggle bits corresponding to row
     Button &= 0x00F;
-    Button = ((P2IN & BIT0) == BIT0) ? (Button | BIT7) : (Button & ~BIT7);
-    Button = ((P2IN & BIT2) == BIT2) ? (Button | BIT6) : (Button & ~BIT6);
-    Button = ((P4IN & BIT0) == BIT0) ? (Button | BIT5) : (Button & ~BIT5);
-    Button = ((P3IN & BIT7) == BIT7) ? (Button | BIT4) : (Button & ~BIT4);
+    Button = ((P2IN & (KEYPAD_ROW1(1))) == (KEYPAD_ROW1(1))) ? (Button | BIT4) : (Button & ~BIT4);
+    Button = ((P2IN & (KEYPAD_ROW2(1))) == (KEYPAD_ROW2(1))) ? (Button | BIT5) : (Button & ~BIT5);
+    Button = ((P4IN & (KEYPAD_ROW3(1))) == (KEYPAD_ROW3(1))) ? (Button | BIT6) : (Button & ~BIT6);
+    Button = ((P4IN & (KEYPAD_ROW4(1))) == (KEYPAD_ROW4(1))) ? (Button | BIT7) : (Button & ~BIT7);
 }//--END CheckRow-----------------------------------------------------------------------------------
 
 //-CheckButton: Call functions to check which button was pressed, save it in last button and debounce.
@@ -386,15 +383,16 @@ __interrupt void EUSCI_B1_I2C_ISR(void) {
 }
 
 //-ISR Port 4 ----------------------------------------------------------------------------
+#pragma vector = PORT1_VECTOR
 #pragma vector = PORT2_VECTOR
 #pragma vector = PORT3_VECTOR
 #pragma vector = PORT4_VECTOR
 __interrupt void ISR_Button_Pressed(void) {
     CheckFlag = true; // Tell system to check which key was pressed
-    P2IFG &= ~BIT0;  // Clear Interrupt Flags
-    P2IFG &= ~BIT2;
-    P4IFG &= ~BIT0;
-    P3IFG &= ~BIT7;
+    GPIO_clearInterrupt(KEYPAD_ROW1(0), KEYPAD_ROW1(1));
+    GPIO_clearInterrupt(KEYPAD_ROW2(0), KEYPAD_ROW2(1));
+    GPIO_clearInterrupt(KEYPAD_ROW3(0), KEYPAD_ROW3(1));
+    GPIO_clearInterrupt(KEYPAD_ROW4(0), KEYPAD_ROW4(1));
 }//-- End ISR_P5_Button_Pressed ----------------------------------------------------------
 
 //-ISR Timer B---------------------------------------------------------------------------
