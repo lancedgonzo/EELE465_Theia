614924a919d452266025a75b11a48c0246078433
Lance G <lancedgonzalez@gmail.com>
2024-04-12 00:49:49 -0600

    added some Peltier updates

--- Project06/Project06-2355/Project06-2355Main.c
+++ Project06/Project06-2355/Project06-2355Main.c
@@ -97,8 +97,8 @@ uint8_t SecondaryState = SecondaryStateInit;
 uint8_t TransmitState = TransmitInit; // 0 LCD 1 LED 2 RTC 3 ADC, 4 pending LCD, 5 pending LED, 6 pending RTC 7 pending ADC
 
 #define TempThreshold 1000.0
-#define PELTIER_HEAT BIT1
-#define PELTIER_COOL BIT0
+#define PELTIER_HEAT BIT0
+#define PELTIER_COOL BIT1
 
 
 // Keypad
@@ -138,9 +138,14 @@ int main(void) {
 
 
     //Peltier INIT
-    P6DIR |= (PELTIER_COOL | PELTIER_HEAT);
-    P6OUT |= (PELTIER_COOL | PELTIER_HEAT);
-
+    P3DIR |= (PELTIER_COOL | PELTIER_HEAT);
+    P3OUT |= (PELTIER_COOL | PELTIER_HEAT);
+    
+    //led for peltier
+    // P6DIR |= BIT6;
+    // P6OUT |= BIT6;
+    // P1DIR |= BIT1;
+    // P1OUT |= BIT1;
     // Disable the GPIO power-on default high-impedance mode
     // to activate previously configured port settings
     PM5CTL0 &= ~LOCKLPM5;
@@ -289,18 +294,22 @@ void PeltierOff() {
 
 //Turns on P2.1 after safety delay, this turns switch connected to heating on
 void PeltierCool() {
-    P6OUT |= PELTIER_HEAT;
-    P6OUT |= PELTIER_COOL;
+    P3OUT |= PELTIER_HEAT;
+    P3OUT |= PELTIER_COOL;
     delay_ms_(50);
-    P6OUT &= ~PELTIER_COOL;
+    P6OUT &= ~BIT6;
+    P1OUT |= BIT1;
+    P3OUT &= ~PELTIER_COOL;
 }
 
 //Turns on P2.0 after safety delay, this turns switch connected to cooling on
 void PeltierHeat() {
-    P6OUT |= PELTIER_HEAT;
-    P6OUT |= PELTIER_COOL;
+    P3OUT |= PELTIER_COOL;
+    P3OUT |= PELTIER_HEAT;
     delay_ms_(50);
-    P6OUT &= ~PELTIER_HEAT;
+    P6OUT |= BIT6;
+    P1OUT &= ~BIT1;
+    P3OUT &= ~PELTIER_HEAT;
 }
 
 //Will compare the current temperature to the set temperature, and turn on the appropriate switch
